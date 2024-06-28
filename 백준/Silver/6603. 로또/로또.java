04893dec11958d07d6a31f9992078b399f05a037
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-06-28 13:30:19 +0900

    [Silver II] Title: 로또, Time: 100 ms, Memory: 13236 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,63 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int k;
+	static int[] arr;
+	static int[] ans;
+	static boolean[] v;
+	static StringBuilder sb;
+	
+	
+	static void com (int index, int str) {
+		if(index==6) {
+			for(int t: ans) sb.append(t+" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = str; i<k; i++) {
+			if(v[i]) continue;
+			v[i] = true;
+			ans[index] = arr[i];
+			
+			com(index+1, i+1);
+			v[i] = false;
+		}
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		StringTokenizer st;
+		sb = new StringBuilder();
+		
+		while (true) {
+			str = br.readLine();
+			st = new StringTokenizer(str);
+			k = Integer.parseInt(st.nextToken());
+			
+			if (k==0) break;
+			
+			arr = new int[k];
+			ans = new int[6];
+			v = new boolean[k];
+			
+			for (int i = 0; i<k; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			com(0,0);
+			
+			sb.append("\n");
+		}
+		
+		br.close();
+		System.out.print(sb);
+		
+	}
+
+}
