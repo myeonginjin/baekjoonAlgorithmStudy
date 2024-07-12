4a110c2f5500d3321001eeeed91ad027047300cc
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-07-12 17:00:24 +0900

    [Silver I] Title: 부분수열의 합, Time: 180 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] a = new int[n];
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		for (int i = 0; i<n; i++) {
+			a[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		boolean[] v = new boolean[2000001];
+		
+		for (int i = 1; i< 1<<n; i++) {
+			int sum = 0;
+			
+			for (int j = 0; j<n; j++) {
+				if((i & 1<<j) != 0) sum += a[j];
+			}
+			if(sum < v.length) v[sum] = true; 
+		}
+		
+		for (int i = 1; i<v.length; i++) {
+			if(!v[i]) {
+				System.out.println(i);
+				break;
+			}
+			
+		}
+	}
+
+}
