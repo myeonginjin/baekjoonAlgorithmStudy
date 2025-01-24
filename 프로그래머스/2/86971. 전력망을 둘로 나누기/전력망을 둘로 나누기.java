b0b724693914abffb754063030a5d63a5f2707da
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-01-24 15:09:05 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 17.47 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<Integer>[] g;
+    static boolean[][] g;
     static boolean[] v;
-    static HashSet<int[]> hs;
     static int answer = Integer.MAX_VALUE;
     static int cnt;
     
     public int solution(int n, int[][] wires) {
-        g = new ArrayList[n+1]; for (int i = 1; i<=n; i++) g[i] = new ArrayList<>();
-        hs = new HashSet<>();
+        g = new boolean[n+1][n+1];
+
         
         for (int i = 0; i<wires.length; i++) {
             int[] t = wires[i];
@@ -17,13 +16,11 @@ class Solution {
             int go = t[0];
             int to = t[1];
             
-            g[go].add(to);
-            g[to].add(go);
-            
-            hs.add(new int[]{go, to});
+            g[go][to] = true;
+            g[to][go] = true;
         }
         
-        for (int[] del : hs) {
+        for (int[] del : wires) {
             
             //초기화
             v = new boolean[n+1];
@@ -35,47 +32,58 @@ class Solution {
             //System.out.printf("go : %d   to : %d   ->>>  ", go, to);
             
             //연결 끊기
-            for (int i = 0; i<g[go].size(); i++) {
-                if(g[go].get(i) == to) {
-                    g[go].remove(i);
-                    break;
-                }
-            }
-            //연결 끊기
-            for (int i = 0; i<g[to].size(); i++) {
-                if(g[to].get(i) == go) {
-                    g[to].remove(i);
-                    break;
-                }
-            }
+            // for (int i = 0; i<g[go].size(); i++) {
+            //     if(g[go].get(i) == to) {
+            //         g[go].remove(i);
+            //         break;
+            //     }
+            // }
+            g[go][to] = false;
             
-            //개수 세기
-            for (int i = 1; i<=n; i++){
-                if(g[i].size()!=0) {
+            //연결 끊기
+            // for (int i = 0; i<g[to].size(); i++) {
+            //     if(g[to].get(i) == go) {
+            //         g[to].remove(i);
+            //         break;
+            //     }
+            // }
+            g[to][go] = false;
+            
+            
+            dfs(go);
+            
+            int group1 = cnt;
+            int group2 = n - cnt;
 
-                    //System.out.print("  start : "+g[i].get(0) +"   ->>>  ");
-                    dfs(g[i].get(0));
+            System.out.println(group1 +" " +group2);
+
+            answer = Math.min( Math.abs( group1 - group2 ) , answer );
+            
+
+            
+//             //개수 세기
+//             for (int i = 1; i<=n; i++){
+//                 if(g[i].size()!=0) {
+
+//                     //System.out.print("  start : "+g[i].get(0) +"   ->>>  ");
+//                     dfs(g[i].get(0));
                     
-                    int group1 = cnt;
-                    int group2 = n - cnt;
+//                     int group1 = cnt;
+//                     int group2 = n - cnt;
                     
-                    answer = Math.min( Math.abs( group1 - group2 ) , answer );
+//                     answer = Math.min( Math.abs( group1 - group2 ) , answer );
                     
                     
-                    break;
-                }
-            }
+//                     break;
+//                 }
+//             }
             
             //연결 다시 하기
-            g[go].add(to);
-            g[to].add(go);
-            
-            
+            // g[go].add(to);
+            // g[to].add(go);
+            g[go][to] = true;
+            g[to][go] = true;
         }
-        
-        
-
-        
         return answer;
     }
     
@@ -83,12 +91,10 @@ class Solution {
         v[node] = true;
         cnt++;
         
-        for (int i = 0; i<g[node].size(); i++) {
-            int next = g[node].get(i);
-                
-            if(v[next]) continue;
+        for (int i = 1; i<g[node].length; i++) {
+            if(!g[node][i] || v[i]) continue;
             
-            dfs(next);
+            dfs(i);
         }
     }
 }
