f9522d988edf2d6c0676327ae88cc2446b9cc82b
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-10 20:57:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 73.69 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,6 +4,11 @@ class Solution {
     public int[] solution(String[] operations) {
         ArrayList<Integer> a = new ArrayList<>();
         
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Integer> rQ = new PriorityQueue<>(
+            (o1, o2) -> { return -Integer.compare(o1, o2); }
+        );
+        
         for (int i = 0; i<operations.length; i++) {
             String[] process = operations[i].split(" ");
             
@@ -11,49 +16,67 @@ class Solution {
             int num = Integer.parseInt(process[1]);
             
             if(type.equals("I")){
-                a.add(num);
+                //a.add(num);
+                q.add(num);
+                rQ.add(num);
             } else if (type.equals("D") && num == -1) {
                 
-                if(a.isEmpty()) continue;
+                if(q.isEmpty()) continue;
                 
-                Collections.sort(a , (o1, o2) -> {
-                   return Integer.compare( o1, o2 ) ;
-                });
-                a.remove(0);
+                // Collections.sort(a , (o1, o2) -> {
+                //    return Integer.compare( o1, o2 ) ;
+                // });
+                // a.remove(0);
+                int min = q.poll();
+                rQ.remove(min);
+                
             } else {
                 
-                if(a.isEmpty()) continue;
+                if(q.isEmpty()) continue;
                 
-                Collections.sort(a , (o1, o2) -> {
-                   return -Integer.compare( o1, o2 ) ;
-                });
-                a.remove(0); 
+                // Collections.sort(a , (o1, o2) -> {
+                //    return -Integer.compare( o1, o2 ) ;
+                // });
+                // a.remove(0); 
+                
+                int max = rQ.poll();
+                q.remove(max);
             }
-            // for (int t  : a) {
-            //     System.out.print(t+" ");
-            // }System.out.println();
+
         }
         
         
         
         int[] answer = new int[2];
         
-        if(a.isEmpty()) {
+        if(q.isEmpty() && rQ.isEmpty()) {
             answer[0] = 0;
             answer[1] = 0;
             }
         else {
-            Collections.sort(a , (o1, o2) -> {
-                   return -Integer.compare( o1, o2 ) ;
-                });
+            // Collections.sort(a , (o1, o2) -> {
+            //        return -Integer.compare( o1, o2 ) ;
+            //     });
+
             
-            if(a.size() >= 2) {
-                answer[0] = a.get(0);
-                answer[1] = a.get(a.size()-1);
-            } else {
-                answer[0] = a.get(0);
-                answer[1] = a.get(0);
+            if(q.isEmpty()) {
+                int n = rQ.poll();
+                answer[0] = n;
+                answer[1] = n;
+            } 
+            
+            if(rQ.isEmpty()) {
+                int n = q.poll();
+                answer[0] = n;
+                answer[1] = n;               
             }
+            
+            else {
+                answer[0] = rQ.poll();
+                answer[1] = q.poll();
+            }
+            
+            
         }
         
 
