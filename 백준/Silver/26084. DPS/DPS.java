6eb8b69f4703884566a780a133f6a24d7a62757d
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-13 14:37:35 +0900

    [Silver II] Title: DPS, Time: 128 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Silver/26084. DPS/DPS.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashMap<Character, Integer> team = new HashMap<>();
+		int[] handle = new int[128];
+		
+		char[] cs = br.readLine().toCharArray();
+		
+		for (int i = 0; i<cs.length; i++) {
+			char c = cs[i];
+			if(team.containsKey(c)) {
+				team.put(c, team.getOrDefault(c, 0) + 1);
+			}
+			
+			else {
+				team.put(c, 1);
+			}
+		}
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i<num; i++) {
+			char c = br.readLine().charAt(0);
+			handle[c]++;
+		}
+		
+		long answer = 1;
+		
+		// 경우의 수 계산 시작
+		out :for (Map.Entry<Character, Integer> em : team.entrySet()) {
+			char c = em.getKey();
+			int r = em.getValue();
+			
+			int n = handle[c];
+
+			
+            long mul = combin(n, r);
+
+            if (mul == 0) {
+                System.out.println(0);
+                return;
+            }
+			
+			answer *= mul;
+		}
+		
+		System.out.println(answer);
+		
+	}
+	
+	
+    // 조합 계산 최적화 (팩토리얼 직접 계산 안 함)
+    static long combin(int n, int r) {
+        if (n < r) return 0;
+        if (r == 0 || n == r) return 1;
+
+        long result = 1;
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+
+}
