9d6b1810e40d942265f1739835439c88bf68c545
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-14 19:02:06 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 12992 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -1,103 +1,49 @@
 
 
 import java.util.*;
-import java.io.*;
 
 public class Main {
 	static int n,m;
-	static int a[];
-	static int b[];
-	static boolean v[];
-	static StringBuilder sb;
+	static int[] a;
+	static int[] b;
+	static StringBuilder sb = new StringBuilder();
 	static HashSet<String> hs = new HashSet<>();
-	static HashSet<String> hs2 = new HashSet<>();
 	
-	static boolean isDuplicate() {
-		int[] c = new int[m];
-		int index  =  0;
-		for (int t : b) c[index++] = t;
-		
-		Arrays.sort(c);
-		String combine = "";
-		
-		for (int t : c) combine += String.valueOf(t) + " ";
-		
-		if(hs.contains(combine)) return true;
-		
-		else {
-			hs.add(combine);
-			return false;
-		}
-	}
-	
-	static void comb(int index) {
+	static void comb(int index, int str) {
 		if(index == m) {
-			if(isDuplicate()) {
-				
-//				String str = "";
-//				for (int t : b) str += String.valueOf( a[t] ) +" "; 
-//				
-//				System.out.println(str);
-				
-				return;
-			}
-			String str = "";
-			for (int t : b) str += String.valueOf( a[t] ) +" "; 
-//			
-//			System.out.println(str);
+			String st = "";
+			for (int t : b) st += String.valueOf(t+" ");
 			
-			if(!hs2.contains(str)) {
-				sb.append(str).append("\n");
-				hs2.add(str);
+			
+			if(!hs.contains(st)) {
+				hs.add(st);
+				sb.append(st).append("\n");
 			}
-		
 			return;
 		}
 		
-		for (int i = 0; i<n; i++) {
-			if(v[i]) continue;
-			
-			b[index] = i;
-			v[i] = true;
-			
-			comb(index+1);
-			
-			v[i] = false;
+		for (int i = str; i<n; i++) {
+			b[index] = a[i];
+			comb(index+1, i+1);
 		}
 	}
 	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str;
-		StringTokenizer st;
-		sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		str = br.readLine();
-		st = new StringTokenizer(str);
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		n = sc.nextInt();
+		m = sc.nextInt();
 		
 		a = new int[n];
 		b = new int[m];
-		v = new boolean[n];
-		
-		str = br.readLine();
-		st = new StringTokenizer(str);
 		
 		for (int i = 0; i<n; i++) {
-			a[i] = Integer.parseInt(st.nextToken()); 
+			a[i] = sc.nextInt();
 		}
 		Arrays.sort(a);
 		
-		
-		comb(0);
-		
-		//System.out.println("======");
+		comb(0,0);
 		System.out.println(sb);
-		
-		br.close();
 	}
 
 }
