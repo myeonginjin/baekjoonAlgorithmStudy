89fe76bffb2eb6efd470f4d8147c059f4b7f75ae
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-14 18:46:15 +0900

    [Gold III] Title: 소문난 칠공주, Time: 280 ms, Memory: 92880 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,89 +1,91 @@
-
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static char[][] a;
-    static ArrayList<int[]> p; // 25개의 좌표 저장
-    static int[][] b; // 선택된 7명
-    static int cnt;
-    static int[] dr = {-1, 1, 0, 0}; // 상하좌우
-    static int[] dc = {0, 0, -1, 1};
-    
-    // 7명이 서로 연결되어 있는지 확인하는 BFS
-    static void check7Prin() {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[5][5];
+	static char[][] c = new char[5][5];
+	static ArrayList<int[]> a = new ArrayList<>();
+	static int[][] b = new int[7][2];
+	static int answer;
+	
+	static void check7() {
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		
+		boolean[] v = new boolean[7];
+		
+		int linkedNodeCnt = 1;
+		
+		
+		
+		int sCnt = c[b[0][0]][b[0][1]] ==  'S' ? 1 : 0; 
+		
+		//if (c[b[0][0]][b[0][1]] == 'S') System.out.println( c[b[0][0]][b[0][1]]);
+		
+		v[0] = true;
+		q.add(new int[] {b[0][0], b[0][1]});
+		
+		while(!q.isEmpty()) {
+			int[] t = q.poll();
+			
+			for (int i = 0; i<7; i++) {
+				if(v[i]) continue;
+				
+				int[] nei = b[i];
+				
+				if( Math.abs(t[0]-nei[0]) + Math.abs(t[1]-nei[1]) == 1) { // 이웃
+					v[i] = true;
+					q.add(new int[] {nei[0], nei[1]});
+					
+					if( c[nei[0]][nei[1]]  == 'S'  ) sCnt++;
+					linkedNodeCnt++;
+				}
+			}
+		}
+		
+		//if(sCnt > 0) System.out.println(linkedNodeCnt +" " + sCnt);
+		
+		if(linkedNodeCnt == 7 && sCnt >= 4) answer++;
+		
+	}
+	
+	
+	static void comb(int index, int str) {
+		if(index == 7) {
+//			for (int[] t  : b) System.out.print(t[0] +" "+t[1] +"  ||  ");
+//			System.out.println();
+			check7();
+			return;
+		}
+		
+		
+		for (int i = str; i<25; i++) {
+			int[] t = a.get(i);
+			
+			b[index][0] = t[0];
+			b[index][1] = t[1];
+			
+			comb(index+1, i+1);
+		}
+	}
 
-        // 첫 번째 노드를 BFS 시작점으로 설정
-        queue.add(b[0]);
-        visited[b[0][0]][b[0][1]] = true;
-        
-        int connectCount = 1; // 연결된 개수
-        int sCount = (a[b[0][0]][b[0][1]] == 'S') ? 1 : 0; // 'S' 개수
-
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int r = cur[0];
-            int c = cur[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < 5 && nc >= 0 && nc < 5 && !visited[nr][nc]) {
-                    for (int[] pos : b) {
-                        if (pos[0] == nr && pos[1] == nc) {
-                            queue.add(new int[]{nr, nc});
-                            visited[nr][nc] = true;
-                            connectCount++;
-                            if (a[nr][nc] == 'S') sCount++;
-                            break; // 이미 b에 있는 값만 찾으면 되므로 break
-                        }
-                    }
-                }
-            }
-        }
-
-        // 모든 7명이 연결되어 있고, 'S'가 4명 이상이면 카운트 증가
-        if (connectCount == 7 && sCount >= 4) {
-            cnt++;
-        }
-    }
-
-    // 25개 좌표 중 7개를 선택하는 조합
-    static void comb(int index, int str) {
-        if (index == 7) {
-            check7Prin(); // 선택된 7명에 대해 연결 여부 검사
-            return;
-        }
-
-        for (int i = str; i < 25; i++) {
-            int[] t = p.get(i);
-            b[index] = t; // 7명 중 한 명 선택
-            comb(index + 1, i + 1);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        a = new char[5][5];
-        p = new ArrayList<>();
-        b = new int[7][2]; // 7명 선택 저장 배열
-
-        // 5x5 입력 받기
-        for (int i = 0; i < 5; i++) {
-            a[i] = sc.next().toCharArray();
-            for (int j = 0; j < 5; j++) {
-                p.add(new int[]{i, j}); // 25개의 좌표 저장
-            }
-        }
-
-        // 25개 중 7명을 선택하는 조합 시작
-        comb(0, 0);
-
-        // 가능한 경우의 수 출력
-        System.out.println(cnt);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		
+		for (int i = 0; i<5; i++) {
+			str = br.readLine();
+			char[] t = str.toCharArray();
+			
+			for (int j = 0; j<5; j++) {
+				c[i][j] = t[j];
+				a.add(new int[] {i,j});
+			}
+		}
+		
+//		for (char[]t : c) {
+//			System.out.print(Arrays.toString(t));
+//		}System.out.println();
+		
+		comb(0, 0);
+		System.out.println(answer);
+	}
 }
