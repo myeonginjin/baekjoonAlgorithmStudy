3ffc63c5ab63be0bbb157a9a4070a3049ba67211
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-14 20:51:22 +0900

    [Silver II] Title: Game Addiction, Time: 100 ms, Memory: 12992 KB -BaekjoonHub

---
+++ 백준/Silver/20152. Game Addiction/Game Addiction.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class Main {
+	static int go, to;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		int go = Math.min(n1, n2);
+		int to = Math.max(n1, n2);
+		
+		long[][] dp = new long[31][31];
+		
+		dp[go][go] = 1;
+		
+		for (int i = go + 1; i<31; i++) {
+			for (int j = go; j<31; j++) {
+				if(i < j) continue;
+				
+				if(i == 0) dp[i][j] = 1;
+				if(j == 0) dp[i][j] = 1;
+				
+				else dp[i][j] = (dp[i-1][j] + dp[i][j-1]);
+			}
+		}
+		
+		System.out.println(dp[to][to]); 
+		
+//		for (int[] t : dp) {
+//			System.out.println(Arrays.toString(t));
+//		}
+		
+		
+	}
+}
