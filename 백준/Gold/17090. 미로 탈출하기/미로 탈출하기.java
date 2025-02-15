cbb76aec05dbb0aaca46eadcb25a96e6326214a1
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-15 14:29:43 +0900

    [Gold III] Title: 미로 탈출하기, Time: 160 ms, Memory: 41800 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
@@ -2,131 +2,75 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n,m;
-	static char[][] a;
-	static boolean[][] can;
-	static boolean[][] cant;
-	static boolean[][] v;
-	static int answer;
-	
-	static boolean canGo(int r, int c) {
-		return r >= 0 && r<n && c >= 0 && c < m;
-	}
-	
-	static boolean dfs(int r, int c) {
-		
-		if(canGo(r,c) && cant[r][c]) return false;
-		
-		if(!canGo(r,c) || can[r][c]) {
-			return true;
-		}
-		
-		if(v[r][c]) return false; //싸이클 발생 방지 
-		v[r][c] = true;
+    static int n, m;
+    static char[][] a;
+    static boolean[][] can;
+    static boolean[][] cant;
+    static boolean[][] visited;
+    static int answer;
 
-		
-		char type = a[r][c];
-		
-		int nr = 0; 
-		int nc = 0;
-		
-		switch(type) {
-		
-		case 'U':
-			nr = r-1;
-			nc = c;
+    static boolean canGo(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < m;
+    }
 
-			if(dfs(nr, nc)) {
-				can[r][c] = true;
-				v[r][c] = false;
-				return true;
-			}
-			//v[r][c] = false;
-			
-			break;
-		case 'R':
-			nr = r;
-			nc = c + 1;
+    static boolean dfs(int r, int c) {
+        if (!canGo(r, c)) return true; // 경계를 벗어나면 탈출 가능
+        if (can[r][c]) return true; // 이미 탈출 가능으로 판정된 경우
+        if (cant[r][c]) return false; // 이미 탈출 불가능으로 판정된 경우
 
-			if(dfs(nr, nc)) {
-				can[r][c] = true;
-				v[r][c] = false;
-				return true;
-			}
-			//v[r][c] = false;
-			
-			break;
-		case 'D':
-			nr = r + 1;
-			nc = c;
-			
-			if(dfs(nr, nc)) {
-				can[r][c] = true;
-				v[r][c] = false;
-				return true;
-			}
-			//v[r][c] = false;
-			
-			break;
-		case 'L':
-			nr = r;
-			nc = c -1;
+        if (visited[r][c]) return false; // 사이클 발생 → 탈출 불가
+        visited[r][c] = true; // 현재 탐색 중인 위치를 방문 처리
 
-			if(dfs(nr, nc)) {
-				can[r][c] = true;
-				v[r][c] = false;
-				return true;
-			}
-			//v[r][c] = false;
-			
-			break;
-		}
-		
-		cant[r][c] = true;
-		
-		return false;
-			 
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str;
-		StringTokenizer st;
-		
-		str = br.readLine();
-		st = new StringTokenizer(str);
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		a = new char[n][m];
-		can = new boolean[n][m];
-		cant = new boolean[n][m];
-		v = new boolean[n][m];
-		
-		for (int i = 0; i<n; i++) {
-			str = br.readLine();
-			char[] t = str.toCharArray();
-			a[i] = t;
-		}
-		
-		for (int i = 0; i<n; i++) {
-			for (int j = 0; j<m; j++) {
-				if(can[i][j]) {
-					answer++;
-					continue;
-				}
-				if(cant[i][j]) continue;
-				
-				if(dfs(i, j)) {
-					answer++;
-					can[i][j] = true;
-				} else {
-					cant[i][j] = true;
-				}
-			}
-		}
-		
-		System.out.println(answer);
-	}
+        int nr = r, nc = c;
+
+        // 방향 설정
+        switch (a[r][c]) {
+            case 'U': nr = r - 1; break;
+            case 'R': nc = c + 1; break;
+            case 'D': nr = r + 1; break;
+            case 'L': nc = c - 1; break;
+        }
+
+        // DFS 탐색
+        boolean canEscape = dfs(nr, nc);
+        if (canEscape) {
+            can[r][c] = true;
+        } else {
+            cant[r][c] = true;
+        }
+
+        visited[r][c] = false; // 백트래킹
+        return canEscape;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        a = new char[n][m];
+        can = new boolean[n][m];
+        cant = new boolean[n][m];
+        visited = new boolean[n][m]; // 방문 체크 배열 추가
+
+        for (int i = 0; i < n; i++) {
+            a[i] = br.readLine().toCharArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (can[i][j]) {
+                    answer++;
+                    continue;
+                }
+                if (!cant[i][j] && dfs(i, j)) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
