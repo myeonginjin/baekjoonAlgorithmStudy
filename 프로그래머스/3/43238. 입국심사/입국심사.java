d1858fe95469089c50b5ce768a3b07bb53d8810b
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-02-22 17:19:30 +0900

    [level 3] Title: 입국심사, Time: 58.79 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        
+        long start = 1;
+        long end = (long)times[0] * n;
+        
+        while( start <= end ) {
+            long mid = (start + end) / 2;
+            long cnt = 0;
+            
+            for (int t  : times) {
+                cnt += mid / t;
+            }
+            
+            if(cnt < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
