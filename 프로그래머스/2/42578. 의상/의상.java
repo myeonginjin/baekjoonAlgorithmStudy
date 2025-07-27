0f2b48928de800cb7d9e39f01e93f2c4b54b52c0
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-07-27 21:05:42 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
 class Solution {
-    static String[][] a;
-    static boolean[] v;
-    static String[] b;
-    static int cnt;
-    HashMap<String, Integer> hm = new HashMap<>();
-    
     public int solution(String[][] clothes) {
         
-        for (int i = 0; i< clothes.length; i++) {
-            String category = clothes[i][1];
-                
-            if(hm.containsKey(category)) {
-                hm.replace(category , hm.get(category) + 1);
-            } else {
-                hm.put(category, 1);
-            }
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for (String[] t : clothes) {
+            String type = t[1];
+            
+            hm.put(type, hm.getOrDefault(type, 0) + 1 );
         }
         
-        int ans = 1;
-        for (Map.Entry<String, Integer> en : hm.entrySet()) {
-            ans *= en.getValue() + 1 ;
+        int answer = 1;
+        
+        
+        for (Map.Entry<String,Integer> t : hm.entrySet() ) {
+            answer *= t.getValue() + 1;
         }
         
-        return ans-1;
+        answer--;
+        
+       
+        return answer;
     }
-    
-    
 }
